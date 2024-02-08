c00e61db782b5a2a97583a663ced229050422bb6
Evilboo <jawednaing@gmail.com>
2024-02-08 21:22:40 +0630

    duplucate email fix

--- src/main/java/controller/RegisterServlet.java
+++ src/main/java/controller/RegisterServlet.java
@@ -79,6 +79,14 @@ public class RegisterServlet extends HttpServlet {
 			dispatcher = request.getRequestDispatcher("register.jsp");
 			dispatcher.forward(request,response);
 		}
+		boolean emailExist = userDAO.checkEmail(email);
+		if(emailExist) {
+			request.setAttribute("status", "emailExit");
+			dispatcher = request.getRequestDispatcher("register.jsp");
+			dispatcher.forward(request,response);
+			//response.sendRedirect("home.jsp");
+			return;
+		}
 		
 		try {
 			
